5556c528c4fd47667a651945f596a256047db12d
“yiping.wu” <yiping.wu@thoughtworks.com>
2022-10-25 16:48:46 +0800

    [feat]: sass config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  module:{
+    rules:[
+          {
+            test: /\.scss$/,
+            loaders:['style','css','sass']
+          }
+    ]
+  }
 })
