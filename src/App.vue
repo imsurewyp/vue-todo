1c4b62fb88b0cfae8b76534bca42f788ed32560f
“yiping.wu” <yiping.wu@thoughtworks.com>
2022-10-25 21:53:23 +0800

    [feat]: toggle task

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,25 @@
         <div class="todo-tasks-wrapper">
           <span>这是待完成tasks</span>
           <ul class="todo-tasks">
-            <li v-for="(item,index) in todoTasks" :key="index">
-              <input type="checkbox" >
+            <li v-for="(item) in todoTasks" :key="item.id">
+              <!-- key值为index时在toggle过程中出现bug，不理解-->
+              <input type="checkbox" v-model="item.done">
               <input type="text" v-model = item.value :disabled="item.isDisabled"
                      @keyup.enter="handleEditInputEnter(item,$event.target.value)">
               <button @click="editTask(item)"> edit </button>
+              <button @click="handleDeleteTask(item)"> delete </button>
             </li>
           </ul>
         </div>
         <div class="completed-tasks-wrapper">
           <span>这是已完成tasks</span>
           <ul class="completed-tasks">
-            <li v-for="(item,index) in completedTasks" :key="index">
-              <input type="checkbox" checked>
+            <li v-for="(item) in completedTasks" :key="item.id">
+              <input type="checkbox" v-model="item.done">
               <input type="text" v-model = item.value  :disabled="item.isDisabled"
                      @keyup.enter="handleEditInputEnter(item,$event.target.value)">
               <button @click="editTask(item)"> edit </button>
+              <button @click="handleDeleteTask(item)"> delete </button>
             </li>
           </ul>
         </div>
@@ -72,6 +75,10 @@ export default {
       const index = this.tasksArr.indexOf(item);
       this.tasksArr[index].value=value;
       this.tasksArr[index].isDisabled=true;
+    },
+    handleDeleteTask:function(item){
+      const index = this.tasksArr.indexOf(item);
+      this.tasksArr.splice(index,1)
     }
   }
 
